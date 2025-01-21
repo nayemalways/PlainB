b92b95d8e6e9f511d6021305accbe3db738fad10
mohammad-nayem <nishanahmed13913@gmail.com>
2025-01-21 11:23:24 +0600

    created app navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import './assets/css/index.css'
+import './assets/css/animate.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
