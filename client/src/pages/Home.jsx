d0cae774203bc24d703bb89699fdfb085dec6dd4
mohammad-nayem <nishanahmed13913@gmail.com>
2025-01-26 20:18:24 +0600

    create Features skeleton

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Layout from '../components/layout/Layout';
 import SliderSkeleton from '../skeleton/SliderSkeleton';
 import HeroSlider from '../components/product/HeroSlider';
+import FeturesSkeleton from '../skeleton/FeturesSkeleton';
 
 const Home = () => {
     return (
         <Layout>              
               {/* <SliderSkeleton/> */}
               <HeroSlider/>
+              <FeturesSkeleton/>
         </Layout>
     );
 };
