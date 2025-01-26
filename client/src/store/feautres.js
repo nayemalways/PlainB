6dde8c772c96fe515b3e676a94dd6328319a4e84
mohammad-nayem <nishanahmed13913@gmail.com>
2025-01-26 21:17:21 +0600

    add product store

---
+++ client/src/store/feautres.js
@@ -0,0 +1,16 @@
+import create from 'zustand';
+import axios from 'axios';
+
+
+const FeaturesStore = create((set) => ({
+    FeaturesList: null,
+    FeaturesListRequest: async () => {
+        let res = await axios.get('api/FeaturesList');
+        if(res.status === 'success') {
+            set({FeaturesStore: res.data['data']});
+        }
+    }
+}))
+
+
+export default FeaturesStore;
