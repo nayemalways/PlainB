2c25c2808756ed7cfc470ca9309b0ea586e3dc96
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-01 12:44:46 +0600

    file rename features to featuresStore.js

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import Layout from '../components/layout/Layout';
 import HeroSlider from '../components/product/HeroSlider';
 import ProductStore  from './../store/productStore';
-import FeaturesStore from './../store/feautres';
+import FeaturesStore from '../store/feautresStore';
  
 
 const Home = () => {
