97564a2308ee00622b92ce20a772f0c60c3f346b
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-01 15:42:42 +0600

    Key added in Brand Components

--- client/src/components/product/Brand.jsx
+++ client/src/components/product/Brand.jsx
@@ -21,8 +21,8 @@ const Brand = () => {
                             {
                                 BrandList.map((item, index) => {
                                     return (
-                                        <div className="col-6 col-lg-8r text-center col-md-8r p-2">
-                                            <Link key={index} to="" className="card h-100 rounded-3 bg-white">
+                                        <div key={index} className="col-6 col-lg-8r text-center col-md-8r p-2">
+                                            <Link  to="" className="card h-100 rounded-3 bg-white">
                                                 <div className="card-body">
                                                     <img className="w-75" src={item?.brandImg}  />
                                                     <p className="bodySmal mt-3">{item?.brandName}</p>
