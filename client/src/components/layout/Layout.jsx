7f336a2bcc845dd82223366814b4c20e7c725abb
mohammad-nayem <nishanahmed13913@gmail.com>
2025-04-28 16:21:09 +0600

    Added react hot toast config

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { Toaster } from 'react-hot-toast';
 
 const Layout = ({children}) => {
     return (
