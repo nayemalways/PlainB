b0436ea931534ab3dcbed5279f4a548e66ec13ec
mohammad-nayem <nishanahmed13913@gmail.com>
2025-04-30 19:33:21 +0600

    Move file into user folder

--- client/src/pages/User/Profile.jsx
+++ client/src/pages/User/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import Layout from '../components/layout/Layout';
-import ProfileForm from '../components/user/ProfileForm';
+import Layout from '../../components/layout/Layout';
+import ProfileForm from '../../components/user/ProfileForm';
 
 const Profile = () => {
     return (
