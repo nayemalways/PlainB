e9b7220b602f22cdca0081a0bcefcafec83f8db7
mohammad-nayem <nishanahmed13913@gmail.com>
2025-05-02 00:15:08 +0600

    added Product Cart functionality

--- client/src/store/cartStore.js
+++ client/src/store/cartStore.js
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import axios from 'axios';
+import { unauthorized } from '../utility/utility';
 
 const CartStore = create((set) => ({
     isCartSubmit : false,
@@ -11,6 +12,33 @@ const CartStore = create((set) => ({
                 [name] : value
             }
            }))
+    },
+
+    // Add to Cart Request
+    SaveCartRequest: async (postbody, productID) => {
+        try {
+            set({isCartSubmit : true});
+            postbody.productID = productID;
+            let res = await axios.post(`/api/SaveProductToCart`, postbody);
+            return res.data["status"] === "Success";
+        }catch(e) {
+            unauthorized(e.response.status);
+        }finally {
+            set({isCartSubmit : false});
+        }
+    },
+
+
+    CartList: null,
+    CartCount: 0,
+    CartListRequest: async () => {
+        try {
+            let res = await axios.get(`/api/SelectCartListProduct`);
+            set({ CartList: res.data["data"]});
+            set({CartCount: res.data['data'].length});
+        }catch(e) {
+            unauthorized(e.response.status);
+        }
     }
 
 }));
