d409b8980dd3b50c74789f14f3996635bb8e5040
mohammad-nayem <nishanahmed13913@gmail.com>
2025-05-06 18:30:25 +0600

    re-desinged navbar

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -55,91 +55,147 @@ const Navbar = () => {
                 </div>
             </div>
 
-            {/* Main nav  */}
-            <nav className="navbar sticky-top bg-white navbar-expand-lg navbar-light py-3">
-                <div className="container">
+            <nav>
+                <div className="container-fluid">
+                    <div className="row d-flex py-3 justify-content-evenly align-items-center">
 
-                    <Link className="navbar-brand" to="/">
-                        <img className="img-fluid" src={logo} alt="plainb" width="96px" />
-                    </Link>
-
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav06" ariacontrols="nav06" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div className="collapse navbar-collapse" id="nav06">
-                        <ul className="navbar-nav mt-3 mt-lg-0 mb-3 mb-lg-0 ms-lg-3">
-                            <span className="nav-item me-4">
-                                <Link className="nav-link" to="/">Home</Link>
-                            </span>
-                        </ul>
-                    </div>
-                    
-                    <div className=" d-lg-flex">
-                        <div className="input-group">
-                            <input 
-                                value={searchKeyword} 
-                                onChange={(e) => setSearchKeyword(e.target.value)} 
-                                className="form-control" 
-                                type="search" 
-                                placeholder="Search" 
-                                aria-label="Search" 
-                                />
-                            <Link 
-                                to={searchKeyword.length > 0 ? `/by-keyword/${searchKeyword}` : '/'}
-                                className="btn btn-outline-dark" 
-                                type="submit">
-                                <svg 
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style=
-                                {{ width: 24, height: 24 }}>
-                                <path 
-                                    strokeLinecap="round" 
-                                    strokeLinejoin="round" 
-                                    strokeWidth={2}
-                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> width: height:
-                                         Slider Component
-                                </svg>
-                            </Link>
+                        {/* Left side  */}
+                        <div className="col-lg-3 col-md-3 col-3">
+                            <div className="text-center">
+                                <Link className="navbar-brand" to="/">
+                                    <img className="img-fluid" src={logo} alt="plainb" width="96px" />
+                                </Link>
+                            </div>
                         </div>
 
+                        {/* Left second  */}
+                        <div className="col-lg-3 col-md-3 col-3">
+                            <ul className='list-unstyled d-flex m-0'>
+                                <li className='nav-link fs-6'>
+                                    <Link className="text-secondary" to="/">
+                                        Home
+                                    </Link>
+                                </li>
+                                <li className='nav-link fs-6'>
+                                    <Link className="text-secondary" to="/about">
+                                        About
+                                    </Link>
+                                </li>
+                                <li className='nav-link fs-6'>
+                                    <Link className="text-secondary" to="/terms">
+                                        Terms
+                                    </Link>
+                                </li>
+                                <li className='nav-link fs-6'>
+                                    <Link className="text-secondary" to="/contact">
+                                        Contact
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
 
-
-                        {
-                            isLogin() ? (
-                               <>
-                               <Link to="/cart" type="button" className="btn ms-2 btn-light position-relative">
-                                    <i className="bi text-dark bi-bag"></i>
-                                    <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success'>
-                                        {CartCount}
-                                        <span className='visually-hidden'>Unread message</span>
-                                    </span>
+                        {/* right second  */}
+                        <div className="col-lg-3 col-md-3 col-3">
+                             <div className="d-flex">
+                                <input 
+                                    value={searchKeyword} 
+                                    onChange={(e) => setSearchKeyword(e.target.value)} 
+                                    className="w-75 px-2 py-1 rounded-2 form-control" 
+                                    type="search" 
+                                    placeholder="Search"  
+                                />
+                                <Link 
+                                    to={searchKeyword.length > 0 ? `/by-keyword/${searchKeyword}` : '/'}
+                                    className="btn btn-outline-dark" 
+                                    type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style=
+                                    {{ width: 24, height: 24 }}>
+                                    <path 
+                                        strokeLinecap="round" 
+                                        strokeLinejoin="round" 
+                                        strokeWidth={2}
+                                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> 
+                                            width: height:
+                                            Slider Component
+                                    </svg>
                                 </Link>
+                             </div>
+                        </div>
 
-                                <Link to="/wish" type="button" className="btn ms-2 btn-light d-flex position-relative">
-                                    <i className="bi text-dark bi-heart"></i>
-                                    <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning'>
-                                        {WishCount}
-                                        <span className='visually-hidden'>Unread message</span>
-                                    </span>
-                                    
-                                    
-                                </Link>
-                               </> 
-                            ) : ("")
-                        }
+                        {/* Right side  */}
+                        <div className="col-lg-3 col-md-3 col-3">
+                            <div className="d-lg-flex">
+                                    {/* User Cart list  */}
+                                        {
+                                            isLogin() ? (
+                                                <span className='nav-item me-4'>
+                                                        <Link to="/cart" type="button" className="btn ms-2 btn-light position-relative">
+                                                            <i className="bi text-dark bi-bag"></i>
+                                                            <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success'>
+                                                                {CartCount}
+                                                                <span className='visually-hidden'>Unread message</span>
+                                                            </span>
+                                                        </Link>
+                                                    </span>
+                                            ) : ""
+                                        }
+                                    {
 
-                        
-
-                        {
-                            isLogin() ? (
-                                <>
-                                    <Link type="button" className="btn ms-3 btn-success d-flex" to="/profile">Profile</Link>
-                                    <Link type="button" onClick={logoutHandle} className="btn ms-3 btn-success d-flex" to="/">Logout</Link>
-                                </>
-                            ) : (
-                                <Link type="button" className="btn ms-3 btn-success d-flex" to="/login">Login</Link>
-                            )
-                        }
+                                    // User Profile 
+                                    isLogin() ? (
+                                        <>
+                                            {/* Profile section  */}
+                                            <div class="btn-group">
+                                                <button 
+                                                    type="button" 
+                                                    class="profile border-success rounded-circle position-relative
+                                                    d-flex justify-content-center align-content-center" 
+                                                    data-bs-toggle="dropdown" 
+                                                    aria-expanded="false"
+                                                >
+                                                    <img className='position-absolute rounded-circle' 
+                                                    src="https://avatars.githubusercontent.com/u/124289808?v=4" alt="" />
+                                                </button>
+                                                <ul class="dropdown-menu">
+                                                    <li className=''>
+                                                        <Link type="button" className="d-flex justify-content-around gap-2 text-center" to="/profile">
+                                                            <i class="bi text-dark bi-person"></i>
+                                                            <span className='text-success'>Profile</span>
+                                                            <span></span>
+                                                        </Link>
+                                                    </li>
+                                                    <li>
+                                                        <Link type="button" className="d-flex justify-content-around gap-2 text-center" to="/profile">
+                                                            <i className="bi text-dark bi-heart"></i>
+                                                            <span className='text-success'>Wishlist</span>
+                                                            <span></span>
+                                                            {/* <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning'>
+                                                                {WishCount}
+                                                                <span className='visually-hidden'>Unread message</span>
+                                                            </span> */}
+                                                        </Link>
+                                                    </li>
+                                                    <li>
+                                                        <Link type="button" className="d-flex justify-content-around gap-2 text-center" to="/order">
+                                                            <i className="bi text-dark bi-truck"></i>
+                                                            <span className='text-success'>Your Order</span>
+                                                            <span></span>
+                                                        </Link>
+                                                    </li>
+                                                    
+                                                    <li><hr class="dropdown-divider"/></li>
+                                                    <li>
+                                                        <Link type="button" onClick={logoutHandle} className="text-center d-block text-danger" to="/">Logout</Link>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </>
+                                    ) : (
+                                        <Link type="button" className="btn ms-3 btn-success d-flex" to="/login">Login</Link>
+                                    )
+                                }
+                            </div>
+                        </div>
                     </div>
                 </div>
             </nav>
@@ -148,3 +204,6 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+ 
