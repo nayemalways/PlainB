17d8b9dfbfced0825e3ab920715b3804139238b7
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-08-31 00:18:45 +0600

    ddf

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // 👈 IMPORTANT for GitHub Pages
   plugins: [react()],
   // server: {
   //   proxy: {
